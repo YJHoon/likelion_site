d0398b4c946687cef926568428f7527463329ff0
YJHoon <yjhoon0816@gmail.com>
2020-03-17 01:17:31 +0900

    change

--- app/models/apply.rb
+++ app/models/apply.rb
@@ -3,10 +3,8 @@ class Apply < ApplicationRecord
 
   belongs_to :recruit
 
-  PARTICIPATION_STATES = %i[participation not late].freeze
-
-  enum additional_a1: PARTICIPATION_STATES, _prefix: :additional_q1
-  enum additional_a2: PARTICIPATION_STATES, _prefix: :additional_q2
+  enum additional_a1: %i[participation not late], _prefix: :additional_q1
+  enum additional_a2: %i[participation not late], _prefix: :additional_q2
 
   validates_format_of :student_id, presence: true, :numericality => true, :with => /20[0-9]{8}/, :message => "알맞은 학번 양식을 사용하세요"
   validates_format_of :phone, presence: true, :numericality => true, :with => /010[0-9]{8}/, :message => "알맞은 전화번호 양식을 사용하세요"
