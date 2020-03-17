2970576d668f35c4dccfec7b848bbb0f72b3b04a
YJHoon <yjhoon0816@gmail.com>
2020-03-18 01:06:20 +0900

    멘토 타입 순서

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @users = User.mentor
+    @president = User.where(mentor_type: "president")
+    @vice_president = User.where(mentor_type: "vice_president")
+    @education = User.where(mentor_type: "education")
+    @promotion = User.where(mentor_type: "promotion")
+    @HI_SW = User.where(mentor_type: "HI_SW")
+    @affairs = User.where(mentor_type: "affairs")
     recruit = Recruit.last if Recruit.last.present?
     if recruit.present? && recruit.start_at < Time.zone.now && recruit.end_at > Time.zone.now
       @recruit = recruit
