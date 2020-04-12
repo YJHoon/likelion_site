a308f35729ffd59369296b52201e450b20ce86c6
YJHoon <yjhoon0816@gmail.com>
2020-04-12 15:52:09 +0900

    action text로 과제 제출

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,7 +2,6 @@ class Submission < ApplicationRecord
   mount_uploader :image, ImageUploader
   mount_uploaders :file, FileUploader
 
-  has_many_attached :file
   has_rich_text :description
 
   belongs_to :user
@@ -13,8 +12,6 @@ class Submission < ApplicationRecord
   has_many :wishes_user, through: :wishes, source: :user
 
   validates :title, presence: true
-  validates :description, presence: true
-  validates :url, presence: true
 
   enum grade: [:normal, :idea, :great]
 
