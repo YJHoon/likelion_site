a0ee73a1ad40414d3ff156c36a888094502412d5
YJHoon <yjhoon0816@gmail.com>
2020-04-13 12:38:07 +0900

    어드민 과제 업로드 사진 크기 변경

--- app/admin/submissions.rb
+++ app/admin/submissions.rb
@@ -46,7 +46,7 @@ ActiveAdmin.register Submission do
   show do
     attributes_table do
       row :title
-      row :description do |submission|
+      row :description, class: 'admin-testImage' do |submission|
         submission.description.html_safe
       end
       row :url
