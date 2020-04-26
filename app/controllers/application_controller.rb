1a7f2e6b80c7bdb6a1c79f7beed416a578a38f22
YJHoon <yjhoon0816@gmail.com>
2020-04-26 21:18:03 +0900

    Revert "어플리케이션 컨트롤러 에디터 헬퍼 추가"
    This reverts commit 881063d9b926500916cd4d01ae49901e9879f357.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  helper ActionText::Engine.helpers
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :prepare_exception_notifier
   
